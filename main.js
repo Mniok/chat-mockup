9c78fd2109e0171e8aa5ec952781053b2ea4627e
Mniok <58875711+Mniok@users.noreply.github.com>
2022-08-17 06:12:41 +0200

    new pot form
    added form to publish new posts

--- main.js
+++ main.js
@@ -4,7 +4,29 @@ const app = Vue.createApp({
 			messages: [
 				{name: 'admin', content: 'This is a message.'},
 				{name: 'admin', content: 'You can post messages using the form below.'}
-			]
+			],
+			newmsg: {name: '', content: ''}
+		}
+	},
+
+	methods: {
+		onSubmit() {
+			if(this.newmsg.name === ''){
+				alert('Please provide name.')
+				return
+			}
+			else if(this.newmsg.content === ''){
+				alert('Cannot send empty message.')
+				return
+			}
+				
+			var newObjMsg = {name: this.newmsg.name, content: this.newmsg.content}
+			//otherwise posted messages remain reactive and change after posting
+			
+
+			this.messages.push(newObjMsg)
+			//this.newmsg.name = '' //don't reset name!
+			this.newmsg.content = ''
 		}
 	}
 })
